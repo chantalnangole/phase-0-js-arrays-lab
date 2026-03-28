db627702d413b2b44f82f0a79fd05fe37459cea4
chantalnangole <chantalnafulacatsandkittens@gmail.com>
2026-03-28 14:42:23 +0300

    added four new functions

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,22 @@
 // Write your code here
+const products = [ "Laptop", "Phone", "Headphones", "Monitor" ];
+function logFirstProduct() {
+  console.log(products[0]);
+}
 
+function addProduct(product) {
+  products.push(product);
+}
+
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 
 // Export the necessary parts for testing
